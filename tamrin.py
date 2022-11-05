14be15f924a5caec87a229811baaee02b0ec5f82
aliquazaimodo <ajali4487@gmail.com>
2022-11-06 01:44:32 +0330

    fix last pro
    fixing

--- tamrin.py
+++ tamrin.py
@@ -1,12 +1,13 @@
 username= input("username:")
+
 password= int(input('password:'))
-if password == 1234:
+if password == 1234 and username=="admin":
     print('welcome')
+    import numpy as np 
+    a = np.array([
+    [1,1,2,2,3,3],
+    [3,3,4,4,5,5]
+    ])
+    print(a)
 else:
-    print('wrong')
-import numpy as np 
-a = np.array([
-[1,1,2,2,3,3],
-[3,3,4,4,5,5]
-])
-print(a)
+  print('wrong')
