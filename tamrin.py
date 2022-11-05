22b9196d7d11406a20f733087cac937250e5d9aa
aliquazaimodo <ajali4487@gmail.com>
2022-11-06 01:19:45 +0330

    add code from last project
    add code

---
+++ tamrin.py
@@ -0,0 +1,12 @@
+username= input("username:")
+password= int(input('password:'))
+if password == 1234:
+    print('welcome')
+else:
+    print('wrong')
+import numpy as np 
+a = np.array([
+[1,1,2,2,3,3],
+[3,3,4,4,5,5]
+])
+print(a)
